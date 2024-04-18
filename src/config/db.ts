23991875a527b649b8f85560c6a85567cbb35d6f
Gumaro Monroy <maromvz@gmail.com>
2024-04-18 16:15:38 -0600

    Add database configuration file

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import colors from 'colors'
+import { exit } from 'node:process'
+
+export const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.DATABASE_URL)
+    console.log(colors.cyan.bold('Conection to database succeeded!'), `${connection.host}:${connection.port}`)
+  } catch (err) {
+    exit(1)
+  }
+}
