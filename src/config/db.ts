a6bb661cd0d00ab863736170969585c6599b0fd4
Gumaro Monroy <maromvz@gmail.com>
2024-04-20 15:04:12 -0600

    Define the database url type for mongoose connection(because strict mode is enabled now)

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import { exit } from 'node:process'
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.DATABASE_URL)
+    const { connection } = await mongoose.connect(process.env.DATABASE_URL as string)
     console.log(colors.cyan.bold('Conection to database succeeded!'), `${connection.host}:${connection.port}`)
   } catch (err) {
     exit(1)
