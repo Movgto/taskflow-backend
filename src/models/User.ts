1252380daa0cc420acc398a202419138076e16e8
Gumaro Monroy <maromvz@gmail.com>
2024-04-20 15:05:58 -0600

    Create the Schema, Type and Model for users

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+interface IUser extends Document {
+  name: string
+  email: string
+  password: string
+  confirmed: boolean
+}
+
+const userSchema : Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  confirmed: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
