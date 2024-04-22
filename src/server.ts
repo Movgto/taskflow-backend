209b28093282d9896bfe9f0b1af03a98f3363549
Gumaro Monroy <maromvz@gmail.com>
2024-04-22 17:25:35 -0600

    Add authentication routes

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,27 @@ import express from 'express'
 import { configDotenv } from 'dotenv'
 import { connectDB } from './config/db'
 import projectRouter from './routes/projectRoutes'
+import authRouter from './routes/authRoutes'
 import cors from 'cors'
 import corsConfig from './config/cors'
 import morgan from 'morgan'
 
 configDotenv()
 
+// Database connection
 connectDB()
 
 const server = express()
 
-server.use(cors(corsConfig))
+// Uncomment the line below to enable CORS rules, see the CORS configuration file as imported on the top
+// server.use(cors(corsConfig))
 
 server.use(morgan('dev'))
 
 server.use(express.json())
 
+// Routes
 server.use('/api/projects', projectRouter)
+server.use('/api/auth', authRouter)
 
 export default server
