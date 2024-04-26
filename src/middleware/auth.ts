f7cbb2375672491307533c85670629eef775f914
Gumaro Monroy <maromvz@gmail.com>
2024-04-26 17:19:23 -0600

    Add authentication middleware for JWT token verification

---
+++ src/middleware/auth.ts
@@ -0,0 +1,47 @@
+import type {Request, Response, NextFunction} from 'express'
+import jwt from 'jsonwebtoken'
+import User, { IUser } from '../models/User'
+import { UserDataJWT } from '../utils/authUtils'
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: IUser
+    }
+  }
+}
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+  const bearer = req.headers.authorization
+
+  if (!bearer) {
+    const error = new Error('Invalid action')
+    return res.status(401).json({
+      error: error.message
+    })
+  }
+
+  const token = bearer.split(' ')[1]
+
+  try {
+    const userData = jwt.verify(token, process.env.JWT_SECRET!)
+  
+    if (typeof userData === 'object' && userData.id) {
+      const user = await User.findById(userData.id).select('_id name email')
+
+      if (user) {
+        req.user = user
+      } else {
+        return res.status(401).json({
+          error: 'Not Authorized'
+        })
+      }
+    }
+  } catch (error) {
+    return res.status(401).json({
+      error: 'Not Authorized'
+    })
+  }
+
+  next()
+}
