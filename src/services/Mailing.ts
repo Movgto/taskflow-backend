3d50dc52546d5589297baa1e2efc70291d4b7846
Gumaro Monroy <maromvz@gmail.com>
2024-04-26 11:49:57 -0600

    Refactor Mailing service to include password reset functionality

--- src/services/Mailing.ts
+++ src/services/Mailing.ts
@@ -1,19 +1,35 @@
 import transport from "../config/nodemailer"
 
-interface IConfirmEmailUser {
+type IMailingService = {
   email: string
   name: string
   token: string
 }
 
 class Mailing {
-  static sendConfirmationEmail = async (user: IConfirmEmailUser) => {
+  static sendConfirmationEmail = async (user: Pick<IMailingService, 'email'|'name'|'token'>) => {
     await transport.sendMail({
       from: 'Taskflow <taskflow@gmail.com>',
       to: user.email,
       subject: 'Taskflow account confirmation',
       text: `Please ${user.name} enter the confirmation code below to verify your account`,
-      html: `<p>Confirmation code: <b>${user.token}</b></p>`
+      html: `<div>
+                <p>Confirmation code: <b>${user.token}</b></p>
+                <a href="${process.env.FRONTEND_URL}/auth/account-confirmation">Confirm account</a>
+              </div>`
+    })
+  }
+
+  static sendResetPasswordInstructions = async (user: Pick<IMailingService, 'email'|'name'|'token'>) => {
+    await transport.sendMail({
+      from: 'Taskflow <taskflow@gmail.com>',
+      to: user.email,
+      subject: 'Taskflow reset password instructions',
+      text: `Please ${user.name} enter the code below to reset your password`,
+      html: `<div>
+                <p>Reset password code: <b>${user.token}</b></p>
+                <a href="${process.env.FRONTEND_URL}/auth/reset-password">Reset password</a>
+              </div>`
     })
   }
 }
