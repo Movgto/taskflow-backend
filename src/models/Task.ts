3afea0350d576d9950fa80d677a4242ae82d65fa
Gumaro Monroy <maromvz@gmail.com>
2024-06-06 18:02:23 -0600

    refactor: Delete associated notes when deleting a task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, Types } from "mongoose"
+import Note from "./Note"
 
 const taskStatus = {
   PENDING: 'pending',
@@ -64,6 +65,14 @@ const TaskSchema : Schema = new Schema({
     }
   ]
 }, { timestamps: true })
+
+TaskSchema.pre('deleteOne', {document: true}, async function() {
+  const taskId = this._id
+
+  if (!taskId) return
+
+  await Note.deleteMany({task: taskId})
+})
 
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 
