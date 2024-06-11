f56162a2d17381c6097b4f4d28d99bec836d610a
Gumaro Monroy <maromvz@gmail.com>
2024-06-10 18:02:47 -0600

    refactor: Update nodemailer configuration to use Gmail service

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -7,8 +7,7 @@ configDotenv()
 console.log(process.env)
 
 const config : () => SMTPTransport.Options = () => ({
-  host: process.env.SMTP_HOST,
-  port: +process.env.SMTP_PORT!,
+  service: 'gmail',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
